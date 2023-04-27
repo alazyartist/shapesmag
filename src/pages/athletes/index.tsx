5b0446bd07e0aa1e07abd01eaf765d4e2ed7f812
Dylan James <alazyartist@gmail.com>
2023-04-26 23:55:41 -0600

    athlete display

--- src/pages/athletes/index.tsx
+++ src/pages/athletes/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { api } from "~/utils/api";
 
 const AthletePage = () => {
-  return <div className="text-xl text-zinc-300">Athlete's</div>;
+  const { data: athletes } = api.Athletes.getAll.useQuery();
+  return (
+    <div className="text-xl text-zinc-300">
+      <div>Athlete's</div>
+      {athletes?.map((athlete) => {
+        return (
+          <div className="flex gap-4">
+            <div>{athlete.name}</div>
+            <div>{athlete.insta}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AthletePage;
