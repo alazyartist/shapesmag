92d2d5d527e36a0fbc2f79f52f1d45f5d7a8c262
Dylan James <alazyartist@gmail.com>
2023-04-28 02:49:36 -0600

    remove scrollab

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -5,7 +5,7 @@ type WrapperProps = {
 };
 const PageWrapper: React.FC<WrapperProps> = ({ children }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center  overflow-hidden bg-gradient-to-b from-[#02246d] to-[#1d2056] text-zinc-300">
+    <main className="no-scrollbar flex min-h-screen flex-col items-center  overflow-hidden bg-gradient-to-b from-[#02246d] to-[#1d2056] text-zinc-300">
       <HeaderMenu />
       <div className="mt-24 flex h-[80vh] w-[95vw] flex-col place-content-center place-items-center overflow-y-scroll ">
         {children}
