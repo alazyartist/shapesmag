94c92a3fbf573f4a63f7f53079e437c146ce0744
Dylan James <alazyartist@gmail.com>
2023-04-28 02:53:16 -0600

    no-scrollbar

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -7,7 +7,7 @@ const PageWrapper: React.FC<WrapperProps> = ({ children }) => {
   return (
     <main className="no-scrollbar flex min-h-screen flex-col items-center  overflow-hidden bg-gradient-to-b from-[#02246d] to-[#1d2056] text-zinc-300">
       <HeaderMenu />
-      <div className="mt-24 flex h-[80vh] w-[95vw] flex-col place-content-center place-items-center overflow-y-scroll ">
+      <div className="no-scrollbar mt-24 flex h-[80vh] w-[95vw] flex-col place-content-center place-items-center overflow-y-scroll ">
         {children}
       </div>
     </main>
