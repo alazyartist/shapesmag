e656a3413a538ac7db258fde8a475ec286c3d4d8
Dylan James <alazyartist@gmail.com>
2023-04-27 22:14:43 -0600

    typing things

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: ["next/core-web-vitals"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -28,6 +28,7 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
+    "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
